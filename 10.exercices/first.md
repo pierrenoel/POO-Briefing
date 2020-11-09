b6be37b0155217fc02a42e2e8834d2ca452e7ec5
pierrenoel <pierrenoel1509@gmail.com>
2020-11-09 16:32:02 +0100

    update

---
+++ 10.exercices/first.md
@@ -0,0 +1,3 @@
+# The Car class
+
+Create the car class which contains attributes and methods.
--- Solved/index.php
+++ Solved/index.php
@@ -1,38 +1,10 @@
-# POO Introduction
+<?php
 
-## What's POO?
+/* 
+Créer la class SendMessage. Cette class va permettre d'envoyer un message identique sur différents réseaux sociaux
+en appellant une seule fonction!
+*/
 
-In the procedural programming, the website uses to read instructions after instructions, line by line. Now with POO everything change.
-In other words, POO means **oriented programming object** So, we are going to use objects and those objects will be working together.
-
-In those chapters, we are going to see some very important points.
-
-### Step by step
-
-- [Introduction](01.Introduction/readme.md)
-- [Construct](02.construct/readme.md)
-- [Methods](03.methods/readme.md)
-- [Heritage](04.heritage/readme.md)
-- [Abstract](05.abstract/readme.md)
-- [Static](06.static/readme.md)
-- [Interface](07.interface/readme.md)
-- [Traits](08.Traits/readme.md)
-
-### Exercices
-- [First](10.exercices/first.md)
-
-### Ressources
-- [PHP.net](https://www.php.net/manual/en/language.oop5.php)
-- [Pierre Giraud](https://www.pierre-giraud.com/php-mysql-apprendre-coder-cours/introduction-programmation-orientee-objet/)
-- [Grafikart](https://www.grafikart.fr/tutoriels/presentation-1091)
-- [PSR](https://www.php-fig.org/psr/)
-  
-Wrote by Pierre for BeCode web development training - 2020
-
-![image](https://media.giphy.com/media/xUNda1SsEtAFU8suM8/giphy.gif)
-
-
-```php
 interface MessageInterface
 {
     public function facebook() : void;
@@ -95,4 +67,3 @@ class MessageFacade
 
 $message = new MessageFacade(new Message('Hello, I am happy to publish this first message'));
 $message->publish();
-```
